911a2476d64a995497925756dce47f0360d429f8
fabezio <fabezio@outlook.fr>
2020-05-29 00:43:10 +0200

    initialized

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let title;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<main>
+	<h1>{title}</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		text-align: justify;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		text-align: center;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
